fdc67866d7f7a5a13237514dc233126f9893a208
aniparu <aniparu122@gmail.com>
2022-10-28 15:07:40 +0530

    some message added...

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Employee {
+	
+	private void empId() {
+		System.out.println("employee id 2345678");
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
